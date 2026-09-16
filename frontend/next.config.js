1dcdf6889a8307fa53539b56172212aa62fba555
Dipto Nath <reachdipto@gmail.com>
2026-09-16 15:34:18 +0530

    fix next.config.js api rewrites for vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/api/:path*`,
       },
       {
         source: '/uploads/:path*',
-        destination: 'http://localhost:8000/uploads/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/uploads/:path*`,
       },
     ]
   },
